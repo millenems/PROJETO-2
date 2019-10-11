743adc9409117cb9ef84d3ea2b04cbdb955e3f98
Millene <millenems06@gmail.com>
2019-10-11 01:27:56 -0300

    working

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,40 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
+router.get('/views/aids', (req, res) => {
+  res.render('aids');
+});
+
+router.get('/views/ansiedade', (req, res) => {
+  res.render('ansiedade');
+});
+
+router.get('/views/avc', (req, res) => {
+  res.render('avc');
+});
+
+router.get('/views/depressao', (req, res) => {
+  res.render('depressao');
+});
+
+router.get('/views/diabetes', (req, res) => {
+  res.render('diabetes');
+});
+
+router.get('/views/mama', (req, res) => {
+  res.render('mama');
+});
+
+router.get('/views/obesidade', (req, res) => {
+  res.render('obesidade');
+});
+
+router.get('/views/toc', (req, res) => {
+  res.render('toc');
+});
+
+router.get('/views/chats', (req, res) => {
+  res.render('chats')
+})
+
 module.exports = router;
